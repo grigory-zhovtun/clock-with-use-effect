a284307946ba61054b7f2cbac0189bca585747eb
Grigory Zhovtun <grigory.zhovtun@gmail.com>
2023-04-08 19:29:07 +0500

    add a little styles

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -5,12 +5,13 @@ const Clock = () => {
 
     useEffect(() => {
         setInterval(() => {
-        const currentTime = new Date()
-        const currenHour = currentTime.getHours()
-        const currentMinutes = currentTime.getMinutes()
-        const currentSeconds = currentTime.getSeconds()
-        const newTime = currenHour + ":" + currentMinutes + ":" + currentSeconds
-        setTime(newTime)
+            const currentTime = new Date()
+            const currenHour = currentTime.getHours() < 10 ? "0" + currentTime.getHours() : currentTime.getHours()
+            const currentMinutes = currentTime.getMinutes() < 10 ? "0" + currentTime.getMinutes() : currentTime.getMinutes()
+            const currentSeconds = currentTime.getSeconds() < 10 ? "0" + currentTime.getSeconds() : currentTime.getSeconds()
+            const newTime = currenHour + ":" + currentMinutes + ":" + currentSeconds
+
+            setTime(newTime)
         }, 1000)
     }, [])
 
