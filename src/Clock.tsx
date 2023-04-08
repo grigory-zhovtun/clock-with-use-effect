d24abd2de9d7f1f446d9c1f1cbc27c872354aa3b
Grigory Zhovtun <grigory.zhovtun@gmail.com>
2023-04-08 22:53:31 +0500

    add addZero function, refactor code

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -3,12 +3,14 @@ import React, {useEffect, useState} from 'react';
 const Clock = () => {
     const [time, setTime] = useState('')
 
+    const addZero = (num: number) => num < 10 ? "0" + num: num
+
     useEffect(() => {
         setInterval(() => {
             const currentTime = new Date()
-            const currenHour = currentTime.getHours() < 10 ? "0" + currentTime.getHours() : currentTime.getHours()
-            const currentMinutes = currentTime.getMinutes() < 10 ? "0" + currentTime.getMinutes() : currentTime.getMinutes()
-            const currentSeconds = currentTime.getSeconds() < 10 ? "0" + currentTime.getSeconds() : currentTime.getSeconds()
+            const currenHour = addZero(currentTime.getHours())
+            const currentMinutes = addZero(currentTime.getMinutes())
+            const currentSeconds = addZero(currentTime.getSeconds())
             const newTime = currenHour + ":" + currentMinutes + ":" + currentSeconds
 
             setTime(newTime)
